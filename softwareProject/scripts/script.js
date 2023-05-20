07ff0e24acb10561c4c6adf65a66d3feee9f9020
eshetu70 <eshetuwek1@gmail.com>
2023-05-20 14:48:25 -0400

    Updated

--- softwareProject/scripts/script.js
+++
@@ -1,9 +0,0 @@
-
-
-window.onload = function () {
-document.getElementById("sub").addEventListener("click", displayDate);
-
-function displayDate() {
-  document.getElementById("demo").innerHTML = Date();
-}
-}
